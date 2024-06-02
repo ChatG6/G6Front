b92ece7cdfc95789ba819baa11a76a5f43c00310
ChatG6 <chatgeneration6@gmail.com>
2024-06-02 22:06:35 +0300

    Fixing UI Design Issue

--- components/editor/EditorToolbar.tsx
+++ components/editor/EditorToolbar.tsx
@@ -903,23 +903,57 @@ export const QuillToolbar = () => {
         <Notify message={verifyMessage} dur={30} display={setIsNotif} />
       )}
 
-      <button
-        style={{ color: "#545ceb" }}
-        className="ql-getarts cite-btn"
-        onClick={handlegetcites}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          fill="currentColor"
-          className="bi bi-book h-6"
-          viewBox="0 0 16 16"
+      <div className="cite-imp">
+        <button
+          style={{ color: "#545ceb" }}
+          className="ql-getarts cite-btn"
+          onClick={handlegetcites}
+          id="citeBTN"
         >
-          <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783" />
-        </svg>
-        <span className="ml-1">Cite</span>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="currentColor"
+            className="bi bi-book h-6"
+            viewBox="0 0 16 16"
+          >
+            <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783" />
+          </svg>
+          <span className="ml-1">Cite</span>
+        </button>
+        {showModalcites && (
+          <Savemodal role="importModal">
+            {/* Cites */}
+            <h1>Saved works</h1>
+            <div className="items">
+              {Cites.map((option, index) => (
+                <button
+                  className="items-list"
+                  key={index}
+                  onClick={() => handleCiteSelect(option)}
+                >
+                  <span className="import-data">
+                    <b>Title:</b>
+                    {option.title}
+                  </span>
+                  <span className="import-data">
+                    <b>Authors:</b>
+                    {option.authors}
+                  </span>
+                  <span className="import-data">
+                    <b>Published:</b>
+                    {option.year}
+                  </span>
+                </button>
+              ))}
+            </div>
+            <div className="controls">
+              <button onClick={handlecancelcite}>Cancel</button>
+            </div>
+          </Savemodal>
+        )}
+      </div>
 
       <span className="ql-formats" style={{ display: "flex" }}>
         <select className="ql-size" defaultValue="normal">
@@ -1247,39 +1281,6 @@ export const QuillToolbar = () => {
               </div>
               <div className="controls">
                 <button onClick={handlecancelart}>Cancel</button>
-              </div>
-            </Savemodal>
-          )}
-        </div>
-        <div className="cont-imp">
-          {showModalcites && (
-            <Savemodal role="importModal">
-              {/* Cites */}
-              <h1>Saved works</h1>
-              <div className="items">
-                {Cites.map((option, index) => (
-                  <button
-                    className="items-list"
-                    key={index}
-                    onClick={() => handleCiteSelect(option)}
-                  >
-                    <span className="import-data">
-                      <b>Title:</b>
-                      {option.title}
-                    </span>
-                    <span className="import-data">
-                      <b>Authors:</b>
-                      {option.authors}
-                    </span>
-                    <span className="import-data">
-                      <b>Published:</b>
-                      {option.year}
-                    </span>
-                  </button>
-                ))}
-              </div>
-              <div className="controls">
-                <button onClick={handlecancelcite}>Cancel</button>
               </div>
             </Savemodal>
           )}
